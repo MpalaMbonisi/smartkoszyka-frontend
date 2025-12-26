53e52272b1c19b10e80b5707e01a75ea46096fc4
Mbonisi Mpala <mpalambonisi63@gmail.com>
2025-12-26 16:45:02 +0100

    refactor(footer-component): rename component selector

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
-  selector: 'app-footer.component',
+  selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
